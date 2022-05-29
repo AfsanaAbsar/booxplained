568b9207cf583a6492e3c9a52b4b90bca32cc1ac
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-05-29 20:39:32 +0600

    router implement done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from '../src/components/Home/Home';
+import About from '../src/components/About/About';
+import Reviews from '../src/components/Reviews/Reviews';
+import Dashboard from '../src/components/Dashboard/Dashboard';
+import Blogs from '../src/components/Blogs/Blogs';
 
 function App() {
   return (
     <div >
-      <h1>hhhhh</h1>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+      </Routes>
     </div>
   );
 }
